9b0ea1b6bf3935a3989051f82dad3d6a51f1f268
muyi <eroosamuyi11451@gmail.com>
2025-09-19 06:39:45 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,13 +1,37 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { ScreenContent } from '~/components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
 
 import './global.css';
+
+const queryClient = new QueryClient()
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function HomeScreen() {
+  return   <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+}
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <QueryClientProvider client={queryClient}>
+        <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+        <StatusBar style="auto" />
+      </QueryClientProvider>
     </>
   );
 }
